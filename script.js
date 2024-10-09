3c011e5d4739dc0cfdb7c3a3884f30b213e21ff0
Raldaron <161851182+Raldaron@users.noreply.github.com>
2024-10-09 13:59:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const backButtonMessages = [
     "No worries, mistakes happen!",
     "Backtracking is part of the journey!",
     "Well, well, well... Look who made a mistake. Don't worry, I won't tell. Promise.",
-    "Winners don't make mistakes..."
+    "Winners don't make mistakes...", 
     "Oh no! You made a mistake, but that's okay! That's why there's a back button!"
 ];
 
